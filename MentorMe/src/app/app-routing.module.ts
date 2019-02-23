c9319f99b16ef73161eaba71934fa55896303eb7
Jason <boulwarej@g.cofc.edu>
2019-02-23 11:08:17 -0500

    added mentor and student pages

--- MentorMe/src/app/app-routing.module.ts
+++ MentorMe/src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'mentor', loadChildren: './pages/mentor/mentor.module#MentorPageModule' },
+  { path: 'student', loadChildren: './pages/student/student.module#StudentPageModule' },
 ];
 
 @NgModule({
